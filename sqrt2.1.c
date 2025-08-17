08ba47b6657b327e3bb4d9d69611620f00bf6e8b
katemaslova-0 <maslova.ekaterina@phystech.edu>
2025-08-17 12:19:37 +0300

    Update sqrt2.1.c

--- sqrt2.1.c
+++ sqrt2.1.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 void calc(double a, double b, double c, double discriminant);
-double get_numbers(double n);
-void check(double a);
+double get_numbers(void);
+int check(double a, double b, double c);
 
 int main(void)
 {
@@ -13,48 +14,56 @@ int main(void)
 	double discriminant;
 	
 	printf("Введите коэффициент при x^2: ");
-	a = get_numbers(a);
-
-	check(a);
-
+	a = get_numbers();
+	
 	printf("Введите коэффициент при x: ");
-	b = get_numbers(b);
+	b = get_numbers();
 
 	printf("Введите свободный член: ");
-	c = get_numbers(c);
-	
-	calc(a, b, c, discriminant);
-	
+	c = get_numbers();
+
+	if (check(a, b, c) == 1)
+		calc(a, b, c, discriminant);
 	
 return 0;
 }
 
-void check(double a)
+
+int check(double a, double b, double c)
 {
-	if (a==0)
-	{
-		printf("Уравнение не является квадратным\n");
-		exit(1);
-	}
+	if (a==0 && b==0 && c==0)
+		printf("Уравнение не имеет бесконечное множество решений\n");
+	else if (a==0 && b==0 && c != 0)
+		printf("Уравнение не имеет решений\n");
+	else if (a==0 && b != 0)
+		printf("Уравнение имеет одно решение: %lf\n", -c/b);
+
+	if (a!=0)
+		return 1;
+	else
+		return 0;
 }
 
-double get_numbers(double n)
+
+double get_numbers(void)
 {
+	double n;
 	if (scanf("%lf", &n) != 1)
 	{
 		printf("Ошибка. Попробуйте ещё раз.");
-		exit(1);
 	}
-	return(n);
+	else
+		return(n);
 }
+
 
 void calc(double a, double b, double c, double discriminant)
 {
-discriminant = b*b - 4 * a * c;
-if (discriminant < 0)
-printf("Уравнение не имеет решений\n");
-if (discriminant == 0)
-printf("Уравнение имеет одно решение: %.2f\n", (-b)/2/a);
-if (discriminant > 0)
-printf("Уравнение имеет два решения: %.2f и %.2f\n", ((-b)+sqrt(discriminant))/2/a, (-b-sqrt(discriminant))/2/a);	
+	discriminant = b*b - 4 * a * c;
+	if (discriminant < 0)
+	printf("Уравнение не имеет решений\n");
+	if (discriminant == 0)
+	printf("Уравнение имеет одно решение: %.2f\n", (-b)/2/a);
+	if (discriminant > 0)
+	printf("Уравнение имеет два решения: %.2f и %.2f\n", ((-b)+sqrt(discriminant))/2/a, (-b-sqrt(discriminant))/2/a);	
 }
