c064d3717981f457615f790e73f0afd29f987098
katemaslova-0 <maslova.ekaterina@phystech.edu>
2025-08-18 15:46:02 +0300

    Update sqrt2.1.c

--- sqrt2.1.c
+++ sqrt2.1.c
@@ -31,14 +31,14 @@ int main(void)
 
 int check(double a, double b, double c)
 {
-	if (a==0 && b==0 && c==0)
+	if (a == 0 && b == 0 && c == 0)
 		printf("Уравнение не имеет бесконечное множество решений\n");
-	else if (a==0 && b==0 && c != 0)
+	else if (a == 0 && b == 0 && c != 0)
 		printf("Уравнение не имеет решений\n");
-	else if (a==0 && b != 0)
+	else if (a == 0 && b != 0)
 		printf("Уравнение имеет одно решение: %lf\n", -c/b);
 
-	if (a!=0)
+	if (a != 0)
 		return 1;
 	else
 		return 0;
@@ -59,12 +59,15 @@ double get_numbers(void)
 
 void calc(double a, double b, double c, double discriminant)
 {
-	discriminant = b*b - 4 * a * c;
+	discriminant = b * b - 4 * a * c;
+	double sqr_d = sqrt(discriminant);
+	
 	if (discriminant < 0)
 	printf("Уравнение не имеет решений\n");
 	if (discriminant == 0)
-	printf("Уравнение имеет одно решение: %.2f\n", (-b)/2/a);
+	printf("Уравнение имеет одно решение: %.2f\n", (- b) / 2 /a);
 	if (discriminant > 0)
-	printf("Уравнение имеет два решения: %.2f и %.2f\n", ((-b)+sqrt(discriminant))/2/a, (-b-sqrt(discriminant))/2/a);	
+	printf("Уравнение имеет два решения: %.2f и %.2f\n", ((- b) + sqr_d) / 2 / a, (- b - sqr_d) / 2 / a);	
 }
+
 
