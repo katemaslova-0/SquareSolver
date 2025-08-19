3b8eebd3f234608c414c45ba31c468fbe94c88ed
katemaslova-0 <maslova.ekaterina@phystech.edu>
2025-08-19 10:58:38 +0300

    Update sqrt2.1.c

--- sqrt2.1.c
+++ sqrt2.1.c
@@ -4,23 +4,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-void calc(double a, double b, double c,  double * ptr_x, double * ptr_x1, double * ptr_x2, double * ptr_discr, double * ptr_sqr_discr);
+int calc(double a, double b, double c,  double * ptr_x, double * ptr_x1, double * ptr_x2);
 int get_number(double * ptr);
 int check(double a, double b, double c);
-void output(double * ptr_x, double * ptr_x1, double * ptr_x2, double *ptr_discr);
+void output(double * ptr_x, double * ptr_x1, double * ptr_x2, int quantity_of_roots);
 
 
 int main(void)
 {
-	double a, b, c, x, x1, x2, discriminant, sqr_discr;
+	double a, b, c, x, x1, x2;
+	int quantity_of_roots;
 	double * ptr_a;
 	double * ptr_b;
 	double * ptr_c;
 	double * ptr_x;
 	double * ptr_x1;
 	double * ptr_x2;
-	double * ptr_discr;
-	double *ptr_sqr_discr;
 	
 	ptr_a = &a;
 	ptr_b = &b;
@@ -28,8 +27,6 @@ int main(void)
 	ptr_x = &x;
 	ptr_x1 = &x1;
 	ptr_x2 = &x2;
-	ptr_discr = &discriminant;
-	ptr_sqr_discr = &sqr_discr;
 
 	
 	printf("Введите коэффициент при x^2: ");
@@ -43,8 +40,8 @@ int main(void)
 			{
 				if (check(a, b, c) == 1)
 				{
-					calc(a, b, c, ptr_x, ptr_x1, ptr_x2, ptr_discr, ptr_sqr_discr);
-					output(ptr_x, ptr_x1, ptr_x2, ptr_discr);
+					quantity_of_roots = calc(a, b, c, ptr_x, ptr_x1, ptr_x2);
+					output(ptr_x, ptr_x1, ptr_x2, quantity_of_roots);
 				}
 			}
 		}
@@ -55,14 +52,14 @@ int main(void)
 
 int check(double a, double b, double c)
 {
-	if (a == 0 && b == 0 && c == 0)
+	if (a == 0.0 && b == 0.0 && c == 0.0)
 		printf("Уравнение не имеет бесконечное множество решений\n");
-	else if (a == 0 && b == 0 && c != 0)
+	else if (a == 0.0 && b == 0.0 && c != 0.0)
 		printf("Уравнение не имеет решений\n");
-	else if (a == 0 && b != 0)
+	else if (a == 0.0 && b != 0.0)
 		printf("Уравнение имеет одно решение: %lf\n", -c/b);
 
-	if (a != 0)
+	if (a != 0.0)
 		return 1;
 }
 
@@ -79,24 +76,34 @@ int get_number(double * ptr)
 }
 
 
-void calc(double a, double b, double c, double * ptr_x, double * ptr_x1, double * ptr_x2, double * ptr_discr, double * ptr_sqr_discr)
+int calc(double a, double b, double c, double * ptr_x, double * ptr_x1, double * ptr_x2)
 {
-	*ptr_discr = b * b - 4 * a * c;
-	*ptr_sqr_discr = sqrt(*ptr_discr);
+	double discriminant;
+	discriminant = b * b - 4 * a * c;
+	double sqr_discr;
+	sqr_discr = sqrt(*ptr_discr);
 	*ptr_x = (- b) / 2 /a;
-	*ptr_x1 = (- b + *ptr_sqr_discr) / 2 / a;
-	*ptr_x2 = (- b - *ptr_sqr_discr) / 2 / a;
+	*ptr_x1 = (- b + sqr_discr) / 2 / a;
+	*ptr_x2 = (- b - sqr_discr) / 2 / a;
+	
+	if (discriminant < 0)
+		return 0;
+	else if (discriminant == 0)
+		return 1;
+	else
+		return 2;
 }
 
-void output(double * ptr_x, double * ptr_x1, double * ptr_x2, double * ptr_discr)
+void output(double * ptr_x, double * ptr_x1, double * ptr_x2, int quantity_of_roots)
 {
-	if (*ptr_discr < 0)
+	if (quantity_of_roots == 0)
 	printf("Уравнение не имеет решений\n");
-	if (*ptr_discr == 0)
+	else if (quantity_of_roots == 1)
 	printf("Уравнение имеет одно решение: %.2f\n", *ptr_x);
-	if (*ptr_discr > 0)
+	else if (quantity_of_roots == 2)
 	printf("Уравнение имеет два решения: %.2f и %.2f\n", *ptr_x1, *ptr_x2);
 }
+
 
 
 
