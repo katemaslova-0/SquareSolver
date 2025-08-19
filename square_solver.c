3f6e3d1ceddc81cab2c594cc4dad5d7f41eee28e
katemaslova-0 <maslova.ekaterina@phystech.edu>
2025-08-19 21:27:20 +0300

    Update square_solver.c
    - Файл переименован в square_solver.c
    - Объединены строки 85-86(ныне 89 строка)
    - Инициализирована sqr_discr сразу после создания
    - Добавлено условие для извлечения корня(происходит только в случае discriminant >= 0)
    - Теперь функция check_quadracity выполняет исключительно проверку квадратичности уравнения - вывод корней в случае линейного уравнения перенесён в отдельную функцию output_linear_roots
    - Для линейных случаев сделаны функции calculate_linear_roots и output_linear_roots

--- square_solver.c
+++ square_solver.c
@@ -5,27 +5,31 @@
 #include <math.h>
 
 int calculate_quadratic_roots(double a, double b, double c, double * ptr_x1, double * ptr_x2);
+int calculate_linear_roots(double b, double c, double * ptr_x);
 int get_coefficients(double * ptr_a, double * ptr_b, double * ptr_c);
 int check_quadraticity(double a);
-void output_quadratic_roots(double x1, double x2, int quantity_of_roots);
-void output_linear_roots(double b, double c);
+void output_quadratic_roots(double x1, double x2, int quantity_of_roots_sqr);
+void output_linear_roots(double x, int quantity_of_roots_lin);
 int coefficient_0_or_not(double n);
 
 
 int main(void)
 {
 	double a, b, c, x1, x2;
-	int quantity_of_roots;
+	int quantity_of_roots_sqr, quantity_of_roots_lin;
 
 	if (get_coefficients(&a, &b, &c) != 0)
 	{
 		if (check_quadraticity(a) == 1)
 		{
-			quantity_of_roots = calculate_quadratic_roots(a, b, c, &x1, &x2);
-			output_quadratic_roots(x1, x2, quantity_of_roots);
+			quantity_of_roots_sqr = calculate_quadratic_roots(a, b, c, &x1, &x2);
+			output_quadratic_roots(x1, x2, quantity_of_roots_sqr);
 		}
 		else
-			output_linear_roots(b, c);
+		{
+			quantity_of_roots_lin = calculate_linear_roots(b, c, &x1);
+			output_linear_roots(x1, quantity_of_roots_lin);
+		}
 	}
 
 	return 0;
@@ -48,14 +52,29 @@ int check_quadraticity(double a)
 		return 0;
 }
 
-void output_linear_roots(double b, double c)
+int calculate_linear_roots(double b, double c, double * ptr_x)
 {
 	if (coefficient_0_or_not(b) == 1 && coefficient_0_or_not(c) == 1)
-		printf("Уравнение имеет бесконечное множество решений\n");
+			return 2;
 	else if (coefficient_0_or_not(b) == 1 && coefficient_0_or_not(c) == 0)
-		printf("Уравнение не имеет решений\n");
+			return 0;
 	else if (coefficient_0_or_not(b) == 0)
-		printf("Уравнение имеет одно решение: %lf\n", -c/b);
+	{
+			*ptr_x = - c / b;
+			return 1;
+	}
+	else
+			return 3;
+}
+
+void output_linear_roots(double x, int quantity_of_roots_lin)
+{
+	if (quantity_of_roots_lin == 2)
+		printf("Уравнение имеет бесконечное множество решений\n");
+	else if (quantity_of_roots_lin == 0)
+		printf("Уравнение не имеет решений\n");
+	else if (quantity_of_roots_lin == 1)
+		printf("Уравнение имеет одно решение: %lf\n", x);
 }
 
 
@@ -101,15 +120,16 @@ int calculate_quadratic_roots(double a, double b, double c, double * ptr_x1, dou
 		return 2;
 }
 
-void output_quadratic_roots(double x1, double x2, int quantity_of_roots)
+void output_quadratic_roots(double x1, double x2, int quantity_of_roots_sqr)
 {
-	if (quantity_of_roots == 0)
+	if (quantity_of_roots_sqr == 0)
 	printf("Уравнение не имеет решений\n");
-	else if (quantity_of_roots == 1)
+	else if (quantity_of_roots_sqr == 1)
 	printf("Уравнение имеет одно решение: %.2f\n", x1);
-	else if (quantity_of_roots == 2)
+	else if (quantity_of_roots_sqr == 2)
 	printf("Уравнение имеет два решения: %.2f и %.2f\n", x1, x2);
 }
+
 
 
 
