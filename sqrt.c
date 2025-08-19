ec7c19dc6483c3e8f2626115ff9d4ace49564ab4
katemaslova-0 <maslova.ekaterina@phystech.edu>
2025-08-19 11:18:52 +0300

    Update sqrt.c
    - Убраны указатели для дискриминанта, его корня и корня x(в случае единственного решения)
    - Переменные дискриминанта и его корня сделаны локальными для функции calc, переменная корня х(в случае единственного решения) убрана совсем
    - Функция calc теперь возращает количество корней и записывает её в quantity_of_roots, которая далее передаётся как аргумент функции output

--- sqrt.c
+++ sqrt.c
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-int calc(double a, double b, double c,  double * ptr_x, double * ptr_x1, double * ptr_x2);
+int calc(double a, double b, double c, double * ptr_x1, double * ptr_x2);
 int get_number(double * ptr);
 int check(double a, double b, double c);
-void output(double * ptr_x, double * ptr_x1, double * ptr_x2, int quantity_of_roots);
+void output(double * ptr_x1, double * ptr_x2, int quantity_of_roots);
 
 
 int main(void)
@@ -17,14 +17,12 @@ int main(void)
 	double * ptr_a;
 	double * ptr_b;
 	double * ptr_c;
-	double * ptr_x;
 	double * ptr_x1;
 	double * ptr_x2;
 	
 	ptr_a = &a;
 	ptr_b = &b;
 	ptr_c = &c;
-	ptr_x = &x;
 	ptr_x1 = &x1;
 	ptr_x2 = &x2;
 
@@ -40,8 +38,8 @@ int main(void)
 			{
 				if (check(a, b, c) == 1)
 				{
-					quantity_of_roots = calc(a, b, c, ptr_x, ptr_x1, ptr_x2);
-					output(ptr_x, ptr_x1, ptr_x2, quantity_of_roots);
+					quantity_of_roots = calc(a, b, c, ptr_x1, ptr_x2);
+					output(ptr_x1, ptr_x2, quantity_of_roots);
 				}
 			}
 		}
@@ -76,13 +74,12 @@ int get_number(double * ptr)
 }
 
 
-int calc(double a, double b, double c, double * ptr_x, double * ptr_x1, double * ptr_x2)
+int calc(double a, double b, double c, double * ptr_x1, double * ptr_x2)
 {
 	double discriminant;
 	discriminant = b * b - 4 * a * c;
 	double sqr_discr;
-	sqr_discr = sqrt(*ptr_discr);
-	*ptr_x = (- b) / 2 /a;
+	sqr_discr = sqrt(discriminant);
 	*ptr_x1 = (- b + sqr_discr) / 2 / a;
 	*ptr_x2 = (- b - sqr_discr) / 2 / a;
 	
@@ -94,15 +91,16 @@ int calc(double a, double b, double c, double * ptr_x, double * ptr_x1, double *
 		return 2;
 }
 
-void output(double * ptr_x, double * ptr_x1, double * ptr_x2, int quantity_of_roots)
+void output(double * ptr_x1, double * ptr_x2, int quantity_of_roots)
 {
 	if (quantity_of_roots == 0)
 	printf("Уравнение не имеет решений\n");
 	else if (quantity_of_roots == 1)
-	printf("Уравнение имеет одно решение: %.2f\n", *ptr_x);
+	printf("Уравнение имеет одно решение: %.2f\n", *ptr_x1);
 	else if (quantity_of_roots == 2)
 	printf("Уравнение имеет два решения: %.2f и %.2f\n", *ptr_x1, *ptr_x2);
 }
+
 
 
 
